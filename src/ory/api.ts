64b6737b5697de40651be308d56b33236a2052e2
Sacramentix <benji.u1225u@gmail.com>
2023-05-14 23:47:03 +0200

    test with native fetch

--- src/ory/api.ts
+++ src/ory/api.ts
@@ -3,6 +3,10 @@ import { Configuration, FrontendApi, IdentityApi, OAuth2Api } from "@ory/client"
 export const oryConfig = new Configuration({
     basePath: import.meta.env.ORY_BASE_PATH,
     accessToken: import.meta.env.ORY_ACCESS_TOKEN,
+    credentials: 'include',
+    headers: {
+        'Accept': 'application/json',
+    },
 });
 
 export const oryOAuth = new OAuth2Api(oryConfig);
