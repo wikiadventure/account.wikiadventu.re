95dd8b0cbfa90a371eda539253e4acc3356d3523
Sacramentix <benji.u1225u@gmail.com>
2023-05-15 14:01:25 +0200

    redirect if no valid flow

--- src/ory/api.ts
+++ src/ory/api.ts
@@ -14,3 +14,8 @@ export const oryOAuth = new OAuth2Api(oryConfig);
 export const oryId = new IdentityApi(oryConfig);
 
 export const ory = new FrontendApi(oryConfig);
+
+export type BrowserFlow = "login" | "registration" | "settings" | "recovery" | "verification";
+
+export const getUrlForFlow = (flow: BrowserFlow, query?: URLSearchParams) =>
+    `${import.meta.env.ORY_BASE_PATH}/self-service/${flow}/browser${query ? `?${query.toString()}` : ""}`;
