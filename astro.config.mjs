f4be17ddd7ab8f44c1426c53bc7aa206eebf085d
Sacramentix <benji.u1225u@gmail.com>
2023-05-30 16:59:57 +0200

    add landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
+import Icons from 'unplugin-icons/vite';
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+        autoInstall: true,
+      }),
+    ],
+  },
 });
